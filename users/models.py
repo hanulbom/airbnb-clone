bfbc94ad3bbe279896e1490f113224a52869937c
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-10-21 23:41:35 +0900

    Edit

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """ Custom User Model """
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(max_length=10, null=True)
+    bio = models.TextField(default="")
