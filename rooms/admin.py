853e87a3a6b4114acf6447b61b68e7090755f031
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-10-30 23:34:21 +0900

    #6.0

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,24 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "bath",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("city", "country")
+
+    search_fields = ("^city", "^host__username")
 
 
 @admin.register(models.Photo)
