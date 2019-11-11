73d4b3f6d73519696278e62401551b4b86f3bcb1
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-11-11 13:52:21 +0900

    #8.5

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -80,6 +81,12 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ """
+    """ Photo Admin Definition """
 
-    pass
+    list_display = ("__str__", "get_thumnail")
+
+    def get_thumnail(self, obj):
+        print(dir(obj.file))
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumnail.short_description = "Thumnail"
