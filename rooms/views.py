588963c24e1dadc756c97c3ef19f7b8fefac6c91
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-11-17 18:12:32 +0900

    #13.0

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -14,4 +15,12 @@ class HomeView(ListView):
 
 class RoomDetail(DetailView):
 
+    """ RoomDetail Definition """
+
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
