7d5db66999da24d31f9d45ce67c5e2f9060bb4f0
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-11-22 16:08:43 +0900

    #16.0 Setting up Mailgun

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+
+#EMAIL Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = "brad@sandboxf86fc00dd0024b699573f99ef3586701.mailgun.org"
+EMAIL_HOST_PASSWORD = "73bcb23c3ae37914d94aa5e8ed0bf599-09001d55-64793116"
