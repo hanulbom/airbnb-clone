f6c9e87ffb5421686ee6830204fdebed557c82c7
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-11-22 14:54:33 +0900

    #15.1 Signup Form part Two

--- users/views.py
+++ users/views.py
@@ -34,5 +34,10 @@ class SignUpView(FormView):
     initial = {"first_name": "Woo", "last_name": "Min", "email": "itn@las.com"}
 
     def form_valid(self, form):
-        
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
         return super().form_valid(form)
