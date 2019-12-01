a3761643a962edc657117b718a8e4ab323985903
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-12-02 00:48:33 +0900

    Edit kakao_callback

--- users/views.py
+++ users/views.py
@@ -154,16 +154,17 @@ def kakao_callback(request):
             raise KakaoException("Can't get authorization code.")
         access_token = token_json.get("access_token")
         profile_request = requests.get(
-            "https://kapi.kakao.com/v1/user/me",
+            "https://kapi.kakao.com/v2/user/me",
             headers={"Authorization": f"Bearer {access_token}"},
         )
         profile_json = profile_request.json()
-        email = profile_json.get("kaccount_email", None)
+        kakao_account = profile_json.get("kakao_account")
+        profile = kakao_account.get("profile")
+        email = kakao_account.get("email", None)
         if email is None:
             raise KakaoException("Please also give me your email")
-        properties = profile_json.get("properties")
-        nickname = properties.get("nickname")
-        profile_image = properties.get("profile_image")
+        nickname = profile.get("nickname")
+        profile_image = profile.get("profile_image_url")
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGING_KAKAO:
@@ -188,5 +189,4 @@ def kakao_callback(request):
         return redirect(reverse("core:home"))
     except KakaoException as e:
         messages.error(request, e)
-        print(e)
         return redirect(reverse("users:login"))
