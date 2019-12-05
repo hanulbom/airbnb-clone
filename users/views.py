530d6eb5fb24a8b7f62c545901929ffb0b966aa9
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-12-05 10:42:02 +0900

    #21.10 Great User Mixins

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -26,11 +25,19 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
             login(self.request, user)
         return super().form_valid(form)
 
+    def get_success_url(self):
+            next_arg = self.request.GET.get("next")
+            if next_arg is not None:
+                return next_arg
+            else:
+                return reverse("core:home")
 
 def log_out(request):
     messages.info(request, f"See you later")
     logout(request)
     return redirect(reverse("core:home"))
+
+    
 
 
 class SignUpView(mixins.LoggedOutOnlyView, FormView):
@@ -201,7 +208,7 @@ class UserProfileView(DetailView):
     context_object_name = "user_obj"
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
 
     model = models.User
     template_name = "users/update-profile.html"
@@ -227,7 +234,7 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
         return form
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(mixins.LoggedInOnlyView, mixins.EmailLoginOnlyView, SuccessMessageMixin, PasswordChangeView):
 
     template_name = "users/update-password.html"
     success_message = "Password Updated"
