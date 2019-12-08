149a19599ef9a9dd32191a0bee52059cf0d9bc6e
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-12-08 17:00:10 +0900

    #23.2 Room Photos part Two

--- rooms/views.py
+++ rooms/views.py
@@ -135,7 +135,10 @@ class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
         return room
 
 class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
-    template_name = "room_photos.html"
+    
+    model = models.Room
+    template_name = "rooms/room_photos.html"
+
     def get_object(self, queryset=None):
         room = super().get_object(queryset=queryset)
         if room.host.pk != self.request.user.pk:
