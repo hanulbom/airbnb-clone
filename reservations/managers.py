1a6b59ca828693b892a02be8fe8f66af34ba273c
WooMin <53186618+hanulbom@users.noreply.github.com>
2019-12-16 17:08:20 +0900

    #24.11 Reservation Detail part One

--- reservations/managers.py
+++ reservations/managers.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class CustomReservationManager(models.Manager):
 
-    def get_or_not(sefl, **kwargs):
+class CustomReservationManager(models.Manager):
+    def get_or_none(self, **kwargs):
         try:
             return self.get(**kwargs)
         except self.model.DoesNotExist:
