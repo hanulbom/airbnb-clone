8d786c0253979af8fa499ffe2f65964540eeb244
WooMin <hanulbom@gmail.com>
2019-12-18 23:28:03 +0900

    #25.4 Saving Fav Rooms

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from rooms import models
+from django.shortcuts import redirect, reverse
+from rooms import models as room_models
+from . import models
 
-def save_room(request):
-    room = models.Room.objects.get_or_none(pk=pk)
+def save_room(request, room_pk):
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
     if room is not None:
-        
+        the_list, _ = models.List.objects.get_or_create(user=request.user, name="My Favourite Houses")
+        the_list.rooms.add(room)
+    return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
